441cab8b9c5862d9d12054f25b6ccbdc3f57abfa
Fedorova Ksenia <fisar544@gmail.com>
2026-01-16 11:04:17 +0900

    Добавила HTML файл и обновила JS код

---
+++ task1.js
@@ -0,0 +1,5 @@
+let name = prompt("Введите свое имя");
+let lastname = prompt("Введите вашу фамилию");
+let age = prompt("Введите свой возраст");
+
+console.log(`Привет ${name}. Ваша фамилия ${lastname}, ваш возраст ${age}`);
